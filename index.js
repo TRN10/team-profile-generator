e4e48e8e805d22136188b73bec6676e08a81238a
Trentan Jurkans <107039183+TRN10@users.noreply.github.com>
2022-10-01 22:37:10 +1000

    question arrays added for manager, engineer and intern

--- index.js
+++ index.js
@@ -1,2 +1,77 @@
+// Import required modules
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// Inquirer question arrays
+// Manager questions
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'managerName',
+        message: "What is the name of the team manager?",
+    },
+    {
+        type: 'input',
+        name: 'managersId',
+        message: "What is your team manager's employee ID?"
+    },
+    {
+        type: 'input',
+        name: 'managerEmail',
+        message: "What is your team manager's email address?"
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What is your team manager's office number?",
+    }
+];
+
+// Engineer questions
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: 'What is the name of the engineer?',
+    },
+    {
+        type: 'input',
+        name: 'engineerId',
+        message: "What is the engineer's employee ID?",
+    },
+    {
+        type: 'input',
+        name: 'engineerEmail',
+        message: "What is the engineer's email address",
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "What is the engineer's GitHub username?"
+    }
+];
+
+// Intern questions
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'internName',
+        message: 'What is the name of the intern?',
+    },
+    {
+        type: 'input',
+        name: 'internId',
+        message: "What is the engineer's employee ID?",
+    },
+    {
+        type: 'input',
+        name: 'internEmail',
+        message: "What is the engineer's email address",
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: "What is the name of the intern's school?"
+    }
+
+];
